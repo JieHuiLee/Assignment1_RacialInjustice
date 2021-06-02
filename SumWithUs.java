f535a8bd3043034dceb284d07adee538dd2444b1
JieHuiLee <81312864+JieHuiLee@users.noreply.github.com>
2021-06-03 03:03:10 +0800

    Update SumWithUs.java

--- SumWithUs.java
+++ SumWithUs.java
@@ -2,29 +2,39 @@ package Assignment1;
 
 public class SumWithUs { //Racial Organization Name as superclass //1.3 User Define Class
 	//1.2 Pre-Define Class 
-	String name = "Sum With Us";
+	String name;
 	String aboutUs = "\nSum With Us congregations use their financial resources to invest in programs and"
 			       + "\ninitiatives that build better relationship between different race in community and"
 			       + "\nalleviation of discrimination.They help to avoid the perpetuate racial injustice.";
+	String mission;
+	String contactNo;
+	String location;
 	
-	String mission = "\nThe Sum With Us community stands up for prohibition of racial injustice and fair."
-		           + "\nHere are some of our current campaigns we're very proud to share with you.";
+	public SumWithUs() { //1.4 Constructor without argument
+		name = "Sum With Us";
+		getName();
+	}
 	
-	String contactNo = "04-981 4652";
-	String location = "Sum With Us NGO Organization, Jalan Ru 1, Changlun, 06050 Bukit Kayu Hitam.";
+	public SumWithUs(String mission) { //1.4 Constructor with 1 argument
+		this.mission = mission;
+		printInfo();
+ 		System.out.println("\nOur Mission: "+ this.mission);
+ 		System.out.println("Here are some of our current campaigns we're very proud to share with you.");
+	}
 	
-	//getName () : String
+	public SumWithUs(String contactNum, String location) { //1.4 Constructor with 2 arguments
+		this.contactNo = contactNum;
+		this.location = location;
+		System.out.println("\nContact No: " + this.contactNo);
+ 		System.out.println("Location  : " + this.location);
+ 		System.out.println("\n===========================================================================");
+	}
+	
 	public void getName(){
 		System.out.println("Welcome to Our Community: "+ name);
 	}
  	
 	public void printInfo () {
  		System.out.println("\nAbout Us: " + aboutUs);
- 		System.out.println("\nOur Mission: "+ mission);
- 		System.out.println("\nContact No: " + contactNo);
- 		System.out.println("\nLocation: " + location);
  	}
 }//end class of SumWithUs
- 	
- 	
-
